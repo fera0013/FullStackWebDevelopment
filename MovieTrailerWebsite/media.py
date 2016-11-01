4179f4ac50eaa51c74ee4bc00ec60349da457826
fera0013 <ralphfehrer@gmail.com>
2016-11-01 09:53:19 +0100

    Typo corrected

--- MovieTrailerWebsite/media.py
+++ MovieTrailerWebsite/media.py
@@ -1,6 +1,6 @@
 import webbrowser
 class Movie(object):
-    """Class containing info about a movie, including title,storyline,poster image and trailer"""
+    """Class that contains info about a movie, including title,storyline,poster and trailer"""
     def __init__(self, movie_title,movie_storyline,poster_image,trailer_youtube):
         self.title=movie_title
         self.storyline=movie_storyline
